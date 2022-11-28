81dcf429624f4867cf50fffd8fbdcb9478750c52
rizkymakruf <armppy8@gmail.com>
2022-11-28 16:55:58 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,95 +1,115 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import {
-  StyleSheet,
-  Text,
-  ScrollView,
   View,
-  Button,
+  Text,
+  FlatList,
+  StyleSheet,
   Image,
-  Platform,
+  ActivityIndicator,
 } from "react-native";
-import Constants from "expo-constants";
-const baseUrl = "https://reqres.in";
-function User({ userObject }) {
-  return (
-    <View>
-      <Image
-        source={{ uri: userObject.avatar }}
-        style={{ width: 128, height: 128, borderRadius: 64 }}
-      />
-      <Text style={{ textAlign: "center", color: "white" }}>
-        {`${userObject.first_name} ${userObject.last_name}`}
-      </Text>
-    </View>
-  );
-}
-export default function App() {
-  const [userId, setUserId] = useState(1);
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setErrorFlag] = useState(false);
-  const changeUserIdHandler = () => {
-    setUserId((userId) => (userId === 3 ? 1 : userId + 1));
-  };
-  useEffect(() => {
-    const source = axios.CancelToken.source();
-    const url = `${baseUrl}/api/users/${userId}`;
-    const fetchUsers = async () => {
-      try {
-        setIsLoading(true);
-        const response = await axios.get(url, { cancelToken: source.token });
-        if (response.status === 200) {
-          setUser(response.data.data);
-          setIsLoading(false);
-          return;
-        } else {
-          throw new Error("Failed to fetch users");
-        }
-      } catch (error) {
-        if (axios.isCancel(error)) {
-          console.log("Data fetching cancelled");
-        } else {
-          setErrorFlag(true);
-          setIsLoading(false);
-        }
-      }
+import Axios from "axios";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      isLoading: true,
+      isError: false,
     };
-    fetchUsers();
-    return () => source.cancel("Data fetching cancelled");
-  }, [userId]);
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.wrapperStyle}>
-        {!isLoading && !hasError && user && <User userObject={user} />}
-      </View>
-      <View style={styles.wrapperStyle}>
-        {isLoading && <Text> Loading </Text>}
-        {!isLoading && hasError && <Text> An error has occurred </Text>}
-      </View>
-      <View>
-        <Button
-          title="Load user"
-          onPress={changeUserIdHandler}
-          disabled={isLoading}
-          style={styles.buttonStyles}
+  }
+
+  // Mount User Method
+  componentDidMount() {
+    this.getGithubUser();
+  }
+
+  //   Get Api Users
+  getGithubUser = async () => {
+    try {
+      const response = await Axios.get(`https://reqres.in/api/users`);
+      this.setState({ isError: false, isLoading: false, data: response.data });
+    } catch (error) {
+      this.setState({ isLoading: false, isError: true });
+    }
+  };
+
+  render() {
+    //  If load data
+    if (this.state.isLoading) {
+      return (
+        <View
+          style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
+        >
+          <ActivityIndicator size="large" color="red" />
+        </View>
+      );
+    }
+    // If data not fetch
+    else if (this.state.isError) {
+      return (
+        <View
+          style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
+        >
+          <Text>Terjadi Error Saat Memuat Data</Text>
+        </View>
+      );
+    }
+    // If data finish load
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data.data}
+          renderItem={({ item }) => (
+            <View style={styles.viewList}>
+              <View>
+                <Image
+                  source={{ uri: `${item.avatar}` }}
+                  style={styles.Image}
+                />
+              </View>
+              <View>
+                <Text style={styles.textItemLogin}>
+                  {item.first_name} {item.last_name}
+                </Text>
+                {/* <Text style={styles.textItemUrl}> {item.html_url}</Text> */}
+              </View>
+            </View>
+          )}
+          keyExtractor={({ id }, index) => index}
         />
       </View>
-    </ScrollView>
-  );
+    );
+  }
 }
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "dodgerblue",
+    padding: 20,
+  },
+  viewList: {
+    height: 100,
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderColor: "#DDD",
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
   },
-  wrapperStyle: {
-    minHeight: 128,
+  Image: {
+    width: 88,
+    height: 80,
+    borderRadius: 40,
   },
-  buttonStyles: {
-    padding: 100,
+  textItemLogin: {
+    fontWeight: "bold",
+    textTransform: "capitalize",
+    marginLeft: 20,
+    fontSize: 16,
+  },
+  textItemUrl: {
+    fontWeight: "bold",
+    marginLeft: 20,
+    fontSize: 12,
+    marginTop: 10,
+    color: "blue",
   },
 });
