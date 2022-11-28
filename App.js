fb41fd7af49b3b66d101a66c8d4970a372d30d31
rizkymakruf <armppy8@gmail.com>
2022-11-28 16:11:09 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+  Button,
+  Image,
+  Platform,
+} from "react-native";
+import Constants from "expo-constants";
+const baseUrl = "https://reqres.in";
+function User({ userObject }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Image
+        source={{ uri: userObject.avatar }}
+        style={{ width: 128, height: 128, borderRadius: 64 }}
+      />
+      <Text style={{ textAlign: "center", color: "white" }}>
+        {`${userObject.first_name} ${userObject.last_name}`}
+      </Text>
     </View>
   );
 }
-
+export default function App() {
+  const [userId, setUserId] = useState(1);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setErrorFlag] = useState(false);
+  const changeUserIdHandler = () => {
+    setUserId((userId) => (userId === 3 ? 1 : userId + 1));
+  };
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const url = `${baseUrl}/api/users/${userId}`;
+    const fetchUsers = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(url, { cancelToken: source.token });
+        if (response.status === 200) {
+          setUser(response.data.data);
+          setIsLoading(false);
+          return;
+        } else {
+          throw new Error("Failed to fetch users");
+        }
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Data fetching cancelled");
+        } else {
+          setErrorFlag(true);
+          setIsLoading(false);
+        }
+      }
+    };
+    fetchUsers();
+    return () => source.cancel("Data fetching cancelled");
+  }, [userId]);
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.wrapperStyle}>
+        {!isLoading && !hasError && user && <User userObject={user} />}
+      </View>
+      <View style={styles.wrapperStyle}>
+        {isLoading && <Text> Loading </Text>}
+        {!isLoading && hasError && <Text> An error has occurred </Text>}
+      </View>
+      <View>
+        <Button
+          title="Load user"
+          onPress={changeUserIdHandler}
+          disabled={isLoading}
+          style={styles.buttonStyles}
+        />
+      </View>
+    </ScrollView>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+  },
+  wrapperStyle: {
+    minHeight: 128,
+  },
+  buttonStyles: {
+    padding: 100,
   },
 });
